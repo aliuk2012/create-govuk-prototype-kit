69dfa9927aec2ef5c0eae20b96a7f350b0ef8bc5
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-05-13 17:15:37 +0100

    First attempt at processing args

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,28 @@
+import arg from 'arg'
+
+function parseArgumentsIntoOptions(rawArgs){
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g' : '--git',
+      '-y': '--yes',
+      '-i': '--install'
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  )
+  return {
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    template: args._[0], //could be department/govuk
+    runInstall: args['--install'] || false
+  }
+}
+
 export function cli(args){
-  console.log(args);
+  let options = parseArgumentsIntoOptions(args)
+  console.log(options);
 }
