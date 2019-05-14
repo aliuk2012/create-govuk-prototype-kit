e0e6d70dc5c7bd6e298b7c9431983c05c9080ec6
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-05-14 07:19:17 +0100

    adding a prompt for missing options

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import arg from 'arg'
+import inquirer from 'inquirer'
 
 function parseArgumentsIntoOptions(rawArgs){
   const args = arg(
@@ -22,7 +23,46 @@ function parseArgumentsIntoOptions(rawArgs){
   }
 }
 
-export function cli(args){
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'JavaScript'
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate
+    }
+  }
+
+  const questions = []
+
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'Please choose which project template to use',
+      choices: ['JavaScript', "TypeScript", "GOV.UK", "HMCTS"]
+    })
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Initialize a git repository',
+      default: false
+    })
+  }
+
+  const answers = await inquirer.prompt(questions)
+
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git
+  }
+}
+
+export async function cli(args){
   let options = parseArgumentsIntoOptions(args)
+  options = await promptForMissingOptions(options)
   console.log(options);
 }
